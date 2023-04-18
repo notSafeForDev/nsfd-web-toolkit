0b2f14bcc720cce1c32f2344ab90f4f8eeeae3cf
notSafeForDev <49533663+notSafeForDev@users.noreply.github.com>
2023-04-18 22:22:27 +0200

    Updated e621API for passing user agent along with requests

--- src/api/e621API/e621API.ts
+++ src/api/e621API/e621API.ts
@@ -1,7 +1,18 @@
 import { postsResponse } from "./e621APITypes";
 
 export class E621API {
+
+    private userAgent: string;
+
+    constructor(userAgent: string) {
+        if (userAgent === "") {
+            throw new Error("Unable to create e621API, user agent can't be left blank");
+        }
+
+        this.userAgent = userAgent;
+    }
+
     async requestPosts(text: string): Promise<postsResponse> {
-        return await fetch("https://e621.net/posts.json?tags=" + text).then(response => response.json());
+        return await fetch("https://e621.net/posts.json?tags=" + text + "&_client=" + this.userAgent).then(response => response.json());
     }
 }
